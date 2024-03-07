be6017b22137e118f4b427158397debc62ed91d1
Heestand, Anton Norman | Anton | GSSD <antonnorman.heestand@rakuten.com>
2024-03-07 19:05:32 +0900

    SVG

--- Package.swift
+++ Package.swift
@@ -17,10 +17,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.7.1"),
+        .package(url: "https://github.com/nicklockwood/SVGPath", from: "1.1.4"),
     ],
     targets: [
         .target(
             name: "Vector",
-            dependencies: ["CoreGraphicsExtensions"]),
+            dependencies: [
+                "CoreGraphicsExtensions",
+                "SVGPath",
+            ]),
     ]
 )
---
+++ Sources/Vector/Resources/SVG.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import CoreGraphics
+import CoreGraphicsExtensions
+import SVGPath
+
+extension VectorPath {
+    
+    public var svgString: String {
+        let svgPath = SVGPath(cgPath: cgPath)
+        let options = SVGPath.WriteOptions(prettyPrinted: true)
+        return svgPath.string(with: options)
+    }
+    
+    public static func svg(
+        path: String
+    ) throws -> VectorPath {
+        
+        let cgPath: CGPath = try .from(svgPath: path)
+        
+        return VectorPath(cgPath: cgPath, closed: true)
+    }
+}
