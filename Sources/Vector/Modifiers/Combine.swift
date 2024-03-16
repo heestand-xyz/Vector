1ca4e53e309e66fc4713d425710b8c44f4cffb22
Anton Heestand <anton.heestand@gmail.com>
2024-03-16 09:20:07 +0900

    Enums

--- Sources/Vector/Modifiers/Combine.swift
+++ Sources/Vector/Modifiers/Combine.swift
@@ -6,7 +6,7 @@ extension VectorPath {
     public static func combine(
         paths: [VectorPath]
     ) -> VectorPath {
-        var cgPath = CGMutablePath()
+        let cgPath = CGMutablePath()
         for vectorPath in paths {
             cgPath.addPath(vectorPath.cgPath)
         }
