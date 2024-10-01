32fb5a412e971e3c471366360b66b7fa354d1a55
Anton Heestand <anton.heestand@gmail.com>
2024-10-01 14:43:10 +0900

    Transform Order

--- Sources/Vector/Modifiers/Transform.swift
+++ Sources/Vector/Modifiers/Transform.swift
@@ -11,11 +11,11 @@ extension VectorPath {
     ) -> VectorPath {
         
         var transform: CGAffineTransform = .identity
-            .translatedBy(x: -anchor.x, y: -anchor.y)
-            .rotated(by: rotation.radians)
-            .scaledBy(x: scale, y: scale)
-            .translatedBy(x: anchor.x, y: anchor.y)
             .translatedBy(x: translation.x, y: translation.y)
+            .translatedBy(x: anchor.x, y: anchor.y)
+            .rotated(by: rotation.radians)
+            .translatedBy(x: -anchor.x, y: -anchor.y)
+            .scaledBy(x: scale, y: scale)
         
         guard let cgPath: CGPath = cgPath.copy(using: &transform) else {
             return .empty
